c1adfd2c04cdd79f32f8c47acdcd0cc88069095e
Divya D Pai <divya.d.pai03@gmail.com>
2023-07-23 17:43:12 +0530

    interior consultant challenge initial commit

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function togglesidebar() {
+    document.getElementsByClassName("sidebar")[0].classList.add("active");
+    console.log("activated sidebar")
+}
+function closesidebar(){
+    document.getElementsByClassName("sidebar")[0].classList.remove("active");
+    console.log("deactivated sidebar")
+   
+}
