740fa0ee56c1bdf812a7704a66c8adb1c40e25f5
Divya D Pai <divya.d.pai03@gmail.com>
2023-09-10 16:38:56 +0530

    removed parcel code

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function togglesidebar() {
-    document.getElementsByClassName("sidebar")[0].classList.add("active");
-    console.log("activated sidebar")
+  document.getElementsByClassName("sidebar")[0].classList.add("active");
+  console.log("activated sidebar");
 }
-function closesidebar(){
-    document.getElementsByClassName("sidebar")[0].classList.remove("active");
-    console.log("deactivated sidebar")
-   
+function closesidebar() {
+  document.getElementsByClassName("sidebar")[0].classList.remove("active");
+  console.log("deactivated sidebar");
 }
